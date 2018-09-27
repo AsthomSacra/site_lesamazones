f72ac2a3a3d222169ce270e34cd848befc6e2b8b
AsthomSacra <43634771+AsthomSacra@users.noreply.github.com>
2018-09-27 21:51:01 +0200

    Add files via upload
    Maj home (html css)

--- dev/src/view/ViewHome.php
+++ dev/src/view/ViewHome.php
@@ -84,6 +84,26 @@
 			</div>
 		</div>
 	</div>
+
+	<footer class="footer">
+		<div class="container">
+			<div class="footer-newsletter">
+				<h1>Newsletter</h1>
+				<form>
+					<input type="email" id="mail" name="user_mail" placeholder="Adresse email" required />
+					<button type="submit">S'inscrire</button>
+				</form>
+			</div>
+			<div class="footer-liens">
+				<h1>Liens utiles</h1>
+				<ul>
+					<li>Test</li>
+					<li>Test</li>
+					<li>Test</li>
+				</ul>
+			</div>
+		</div>
+	</footer>
 	
 	
 
