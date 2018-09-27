9da6eccbf363725d3ec8f344ea740aa9f0e5313a
AsthomSacra <43634771+AsthomSacra@users.noreply.github.com>
2018-09-27 21:35:37 +0200

    Add files via upload
    Maj Home page + css home page

--- dev/src/view/ViewHome.php
+++ dev/src/view/ViewHome.php
@@ -41,7 +41,47 @@
 	<div class="informations">
 		<div class="informations-before"></div>
 		<div class="container">
-			<p class="informations-texte">Et licet quocumque oculos flexeris feminas adfatim multas spectare cirratas, quibus, si nupsissent, per aetatem ter iam nixus poterat suppetere liberorum, ad usque taedium pedibus pavimenta tergentes iactari volucriter gyris, dum exprimunt innumera simulacra, quae finxere fabulae theatrales.</p>
+			<div class="informations-texte">
+				<p>Et licet quocumque oculos flexeris feminas adfatim multas spectare cirratas, quibus, si nupsissent, per aetatem ter iam nixus poterat suppetere liberorum, ad usque taedium pedibus pavimenta tergentes iactari volucriter gyris, dum exprimunt innumera simulacra, quae finxere fabulae theatrales.</p>
+			</div>
+			<div class="informations-image">
+				<img class="informations-image-img" src="img/informations-lesamazones" alt="Les Amazones ?"/>
+			</div>
+		</div>
+	</div>
+
+	<div class="middle">
+		<div class="middle-actus">
+			<div class="container">
+				<h2 class="middle-actus-titre">Actualités</h2>
+				<article class="article">
+					<h3 class="article-titre">Article test</h3>
+					<p class="article-auteur">Asthom</p>
+					<p class="article-date">27-09-2018</p>
+					<p class="article-texte">Hae duae provinciae bello quondam piratico catervis mixtae praedonum a Servilio pro consule missae sub iugum factae sunt vectigales. et hae quidem regiones velut in prominenti terrarum lingua positae ob orbe eoo monte Amano disparantur.</p>
+				</article>
+				<article class="article">
+					<h3 class="article-titre">Article test</h3>
+					<p class="article-auteur">Asthom</p>
+					<p class="article-date">27-09-2018</p>
+					<p class="article-texte">Hae duae provinciae bello quondam piratico catervis mixtae praedonum a Servilio pro consule missae sub iugum factae sunt vectigales. et hae quidem regiones velut in prominenti terrarum lingua positae ob orbe eoo monte Amano disparantur.</p>
+				</article>
+				<article class="article">
+					<h3 class="article-titre">Article test</h3>
+					<p class="article-auteur">Asthom</p>
+					<p class="article-date">27-09-2018</p>
+					<p class="article-texte">Hae duae provinciae bello quondam piratico catervis mixtae praedonum a Servilio pro consule missae sub iugum factae sunt vectigales. et hae quidem regiones velut in prominenti terrarum lingua positae ob orbe eoo monte Amano disparantur.</p>
+				</article>
+			</div>
+		</div>
+		<div class="middle-sidebar-right">
+			<div class="container">
+				<p>Equitis Romani autem esse filium criminis loco poni ab accusatoribus neque his iudicantibus oportuit neque defendentibus nobis. Nam quod de pietate dixistis, est quidem ista nostra existimatio, sed iudicium certe parentis; quid nos opinemur, audietis ex iuratis; quid parentes sentiant, lacrimae matris incredibilisque maeror, squalor patris et haec praesens maestitia, quam cernitis, luctusque declarat.</p>
+
+				<p>Raptim igitur properantes ut motus sui rumores celeritate nimia praevenirent, vigore corporum ac levitate confisi per flexuosas semitas ad summitates collium tardius evadebant. et cum superatis difficultatibus arduis ad supercilia venissent fluvii Melanis alti et verticosi, qui pro muro tuetur accolas circumfusus, augente nocte adulta terrorem quievere paulisper lucem opperientes. arbitrabantur enim nullo inpediente transgressi inopino adcursu adposita quaeque vastare, sed in cassum labores pertulere gravissimos.</p>
+
+				<p>Ideo urbs venerabilis post superbas efferatarum gentium cervices oppressas latasque leges fundamenta libertatis et retinacula sempiterna velut frugi parens et prudens et dives Caesaribus tamquam liberis suis regenda patrimonii iura permisit.</p>
+			</div>
 		</div>
 	</div>
 	
